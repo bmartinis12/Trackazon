8080a4e5af2e1f63f970fb5f4306cf5e00528ac0
Benjamin Martinis <martinisbenjamin@gmail.com>
2023-10-29 00:26:06 -0500

    adjust node mailer

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -100,8 +100,7 @@ export const sendEmail = async (emailContent: EmailContent, sentTo: string[]) =>
     subject: emailContent.subject,
   };
 
-  transporter.sendMail(mailOptions, (error: any, info: any) => {
-    if (error) return console.log(error);
-    console.log('Email sent: ', info);
-  })
+  await transporter.sendMail(mailOptions);
+
+  return {};
 };
